72693655d28680cd87367494b7a5d168ad0de813
Brett Laptop <tri11paragon@tpgc.me>
2025-07-31 19:28:23 -0400

    parker has work

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,25 @@ struct TokenIter<'a> {
     iter: Peekable<Iter<'a, tokenizer::Token>>
 }
 
+trait ToTokenIter<'a> {
+    type IterResult;
+    fn to_token_iter(&'a mut self) -> Self::IterResult;
+}
+
+impl<'a> ToTokenIter<'a> for Vec<tokenizer::Token> {
+    type IterResult = TokenIter<'a>;
+    fn to_token_iter(&'a mut self) -> Self::IterResult {
+        TokenIter::new(self)
+    }
+}
+
 impl<'a> TokenIter<'a> {
+    fn new(vec: &'a mut Vec<tokenizer::Token>) -> Self {
+        TokenIter {
+            iter: vec.iter().peekable()
+        }
+    }
+
     fn has_next(&mut self) -> bool {
         self.iter.peek().is_some()
     }
@@ -82,23 +100,56 @@ struct Parser {
 }
 
 impl Parser {
-    fn program(iter: &mut TokenIter) -> Result<ASTNodes, ASTError> {
+    fn parse_function(iter: &mut TokenIter) -> Result<ASTNodes, ASTError> {
+
+    }
+
+    fn parse_class(iter: &mut TokenIter) -> Result<ASTNodes, ASTError> {
+
+    }
+
+    fn parse_program(iter: &mut TokenIter) -> Result<ASTNodes, ASTError> {
         let nodes = ASTNodes::new();
 
         while iter.has_next() {
+            let mut results = Parser::parse_function(iter);
+            match results {
+                Ok(result) => nodes.append(results),
+                Err(e) => return Err(e)
+            }
             
         }
 
         Ok(nodes)
     }
 
-    fn parse(&mut self, iter: &mut TokenIter) {}
+    fn parse(&mut self, mut iter: TokenIter) {
+        let mut results = Parser::parse_program(&mut iter);
+        match &mut results {
+            Ok(result) => {
+                self.tree.append(result);
+            }
+            Err(e) => {
+                println!("Parser Error: {}", e.message);
+            }
+        }
+        
+    }
+
+    fn new() -> Self {
+        Parser {
+            tree: ASTNodes::new()
+        }
+    }
 }
 
 fn main() {
     let mut tokenizer = Tokenizer::new("var silly = 32;");
 
     tokenizer.tokenize();
+
+    let mut parser = Parser::new();
+    parser.parse(tokenizer.tokens.to_token_iter());
 
     println!("{:?}", &mut tokenizer.tokens);
 }
